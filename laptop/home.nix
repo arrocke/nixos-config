9fc75d65dd0ec0f4500c2079d39975db60da89ca
Adrian Rocke <me@adrianrocke.com>
2026-03-08 22:58:53 -0500

    wayland screenshots

--- laptop/home.nix
+++ laptop/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   home.username = "adrian";
@@ -9,7 +9,19 @@
   wayland.windowManager.sway = {
     enable = true;
 
-    config = {
+    config = let
+      modifier = config.wayland.windowManager.sway.config.modifier;
+    in {
+      keybindings = lib.mkOptionDefault {
+        # Super + Shift + S
+        # Screenshot a selection that saves to ~/Screenshots and copies to clipboard.
+        "${modifier}+Shift+s" = "exec selection=$(slurp) && grim -g \"$selection\" - | tee ~/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png | wl-copy";
+      
+        # Print Screen Button
+        # Screenshot the currently focused screen, save to ~/Screenshots and copy to clipboard.
+        "Print" = "exec grimshot save output - | tee ~/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png | wl-copy";
+      };
+
       fonts = {
         names = [ "InconsolataGo Nerd Font Mono" ];
         size = 10.0;
@@ -79,6 +91,11 @@
     neovim
     gcc # needed for treesitter
     ripgrep # needed for telescope
+
+    # for screenshots
+    grim
+    slurp
+    sway-contrib.grimshot
   ];
 
   programs.git = {
